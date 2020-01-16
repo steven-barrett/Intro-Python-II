40051bd8814895a46b399bde8a568fd20602746f
Steve Barrett <steven122185@gmail.com>
2020-01-16 17:52:25 -0500

    Completed MVP and some extra item functionality to randomize whats in the room

--- src/room.py
+++ src/room.py
@@ -10,27 +10,3 @@ class Room:
 
     def __str__(self):
         return f'Room name: {self.name}. description: {self.description}'
-# class Room:
-#     def __init__(self, name, desc):
-#         self.name = name
-#         self.desc = desc        
-#         self.n_to = None
-#         self.s_to = None
-#         self.e_to = None
-#         self.w_to = None
-
-#     def __repr__(self):
-#         room = f"(Room({repr(self.name)}, {repr(self.description)}))"
-#         return room
-
-#     def __str__(self):
-#         s = f"Room({self.name})"
-#         return s
-
-#     def description(self):
-#         desc = f"{self.description}"
-#         return desc
-
-#     def getName(self):
-#         name = f"{self.name}"
-#         return name
